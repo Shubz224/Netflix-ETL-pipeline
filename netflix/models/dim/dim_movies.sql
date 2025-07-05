1c4ad477ded5945489317be252d56fd428d256b6
Shubham Nikam <nikamshubham224@gmail.com>
2025-07-05 19:55:14 +0530

    fct and dim tables for analysis

---
+++ netflix/models/dim/dim_movies.sql
@@ -0,0 +1,11 @@
+
+//refereance the stagging file created as view
+WITH src_movies AS (
+    SELECT * FROM {{ref('src_movies')}} 
+)
+SELECT
+    movie_id,
+    INITCAP(TRIM(title)) AS movie_title,
+    SPLIT(genres,'|') AS genre_array,
+    genres
+FROM src_movies
